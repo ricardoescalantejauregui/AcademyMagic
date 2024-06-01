2d3b9ebc2d58a1e13c4fa6b488fada55fd19c618
Ricardo Escalante <ricardo.jauregui@gmail.com>
2024-06-01 16:56:41 -0600

    Cambio en base de datos para vercel

--- config.py
+++ config.py
@@ -2,12 +2,25 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adivina'
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+#         'sqlite:///' + os.path.join(basedir, 'app.db')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SWAGGER = {
+#         'title': 'Grimoire Registry API',
+#         'uiversion': 3
+#     }
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adivina'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRES_URL')
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SWAGGER = {
-        'title': 'Grimoire Registry API',
-        'uiversion': 3
-    }
+
+# class TestConfig(Config):
+#     TESTING = True
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
