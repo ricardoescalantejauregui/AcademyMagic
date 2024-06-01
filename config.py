f7cbad4e0ab830c83d231d9bb760eb7f2b8c4cd4
Ricardo Escalante <ricardo.jauregui@gmail.com>
2024-06-01 17:08:23 -0600

    Cambio variable de ambiente

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adivina'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRES_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
