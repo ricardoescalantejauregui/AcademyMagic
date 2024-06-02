70de772a3a320c7494f235501fce4f54f047d944
Ricardo Escalante <ricardo.jauregui@gmail.com>
2024-06-01 18:09:57 -0600

    Cambio variable de ambiente y conexion a postgres

--- config.py
+++ config.py
@@ -2,25 +2,11 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adivina'
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-#         'sqlite:///' + os.path.join(basedir, 'app.db')
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     SWAGGER = {
-#         'title': 'Grimoire Registry API',
-#         'uiversion': 3
-#     }
-
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'adivina'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# class TestConfig(Config):
-#     TESTING = True
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SWAGGER = {
+            'title': 'Grimoire Registry API',
+            'uiversion': 3
+        }
